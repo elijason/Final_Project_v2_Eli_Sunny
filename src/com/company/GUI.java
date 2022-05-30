37d0100a124298f9aef8169b8e98eb4ac36c101f
sunbunfun <99900086+sunbunfun@users.noreply.github.com>
2022-05-30 00:55:34 -0700

    changes

---
+++ src/com/company/GUI.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame {
+    public GUI() {
+        frame();
+    }
+
+    public void frame() {
+
+        JFrame f = new JFrame("Bill Splitter");
+
+        f.setVisible(true);
+        f.setSize(400, 400);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel p = new JPanel();
+        JButton b1 = new JButton("Calculate A Bill");
+        b1.setBounds(50, 130, 300, 25);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == b1) {
+                    f.dispose();
+                    BillSplit billSplitWindow = new BillSplit();
+                }
+            }
+        });
+
+        JButton b2 = new JButton("Exit Application");
+        b2.setBounds(50, 160, 300, 25);
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == b2) {
+                    System.exit(0);
+                }
+            }
+        });
+
+        JLabel labelOne = new JLabel("Welcome to the Bill Splitter!");
+        labelOne.setBounds(130, 5, 300, 200);
+
+        JButton blue = new JButton("Blue");
+        blue.setBounds(20, 300, 70, 25);
+        blue.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                p.setBackground(Color.BLUE);
+                labelOne.setForeground(Color.WHITE);
+                f.setBackground(Color.BLUE);
+            }
+        });
+
+        JButton white = new JButton("White");
+        white.setBounds(110, 300, 70, 25);
+        white.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                p.setBackground(Color.WHITE);
+                labelOne.setForeground(Color.BLACK);
+                f.setBackground(Color.WHITE);
+            }
+        });
+
+        JButton black = new JButton("Black");
+        black.setBounds(200, 300, 70, 25);
+        black.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                p.setBackground(Color.BLACK);
+                labelOne.setForeground(Color.WHITE);
+                f.setBackground(Color.BLACK);
+            }
+        });
+
+        p.setLayout(null);
+        p.add(b1);
+        p.add(b2);
+        p.add(blue);
+        p.add(white);
+        p.add(black);
+        p.add(labelOne);
+        f.add(p);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,12 @@
 package com.company;
 
-import javax.swing.*;
-import java.awt.*;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Final Project Eli and Sunny"); //hello
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-
-        //Creating the MenuBar and adding components
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("Bill");
-        JMenu m2 = new JMenu("Help");
-        mb.add(m1);
-        mb.add(m2);
-        JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m22 = new JMenuItem("Save as");
-        m1.add(m11);
-        m1.add(m22);
-
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
-
-        // Text Area at the Center
-        JTextArea ta = new JTextArea();
-
-        //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.getContentPane().add(BorderLayout.CENTER, ta);
-        frame.setVisible(true);
+        GUI home = new GUI();
 
         Scanner sc = new Scanner(System.in);
 
@@ -52,7 +17,6 @@ public class Main {
         } else if (Objects.equals(equalReponse, "not equal")) {
             individualizedBill(sc);
         }
-
     }
 
     public static void equalSplit(Scanner sc) {
